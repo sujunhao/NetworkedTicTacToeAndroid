c3b575d84899c714dc526a73b42cbde323e535fc
hipNcoolJule <julian.clayton.jc@gmail.com>
2015-09-15 20:20:19 -0400

    Removed duplicated directory

--- COMP2601A2/COMP2601A2Server--100892373/src/edu/carleton/COMP2601/a2/EventHandler.java
+++
@@ -1,9 +0,0 @@
-package edu.carleton.COMP2601.a2;
-import org.json.JSONException;
-
-/**
- * Created by julian on 2015-01-29.
- */
-public interface EventHandler {
-    public void handleEvent(Event event) throws JSONException;
-}
